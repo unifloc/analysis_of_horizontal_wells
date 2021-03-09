25b49614c72ded919e443db87281cc207848feb5
SvoykinEV <57312403+sevrn73@users.noreply.github.com>
2021-03-10 01:04:16 +0300

    Переработка тетрадей

--- model_create.py
+++ model_create.py
@@ -8,6 +8,8 @@ from plotly.offline import iplot
 from jinja2 import Environment, FileSystemLoader
 from ecl2df import grid, EclFiles
 import plotly.express as px
+import math as m
+from scipy.interpolate import interp1d
 
 """
 @alexeyvodopyan
@@ -32,7 +34,7 @@ class ModelGenerator:
                  rezim='ORAT', prod_bhp=None, horizontal=False, y_stop=None, only_prod=False,
                  lgr=False, lx=None, ly=None, cells_cy=None, cells_v=None, cells_cx=None,
                  upr_rezim_water=False, upr_rezim_gas=False, rw=None, template=1, neogr=False,
-                 grp=False, nz_grp=1, xs_start_grp=1, xs_stop_grp=2, ys_grp=1, k_grp=100):
+                 grp=False, nz_grp=1, xs_start_grp=1, xs_stop_grp=2, ys_grp=1, k_grp=100, roughness=False):
         # продолжительность расчета
         self.start_date = f'{start_date}'
         self.tstep = f'{mounths}*{days}'
@@ -118,17 +120,16 @@ class ModelGenerator:
             self.poro_box += f'1 {nx} 1 {ny} 1 {nz} /'
         self.por = ''
         if self.upr_rezim_water and self.upr_rezim_gas and not self.neogr:
-            self.por = str(nx*ny) + '*100' + ' ' + str(nx*ny*(nz-2)) + '*' + str(por) + ' ' + str(nx*ny) + '*100' +  ' /'
+            self.por = str(nx*ny) + '*1000' + ' ' + str(nx*ny*(nz-2)) + '*' + str(por) + ' ' + str(nx*ny) + '*1000' +  ' /'
         elif self.upr_rezim_gas and not self.upr_rezim_water and not self.neogr:
-            self.por = str(nx*ny) + '*100' + ' ' + str(nx*ny*(nz-1)) + '*' + str(por) +  ' /'
+            self.por = str(nx*ny) + '*1000' + ' ' + str(nx*ny*(nz-1)) + '*' + str(por) +  ' /'
         elif self.upr_rezim_water and not self.upr_rezim_gas and not self.neogr:
-            self.por = str(nx*ny*(nz-1)) + '*' + str(por) + ' ' + str(nx*ny) + '*100' +  ' /'
+            self.por = str(nx*ny*(nz-1)) + '*' + str(por) + ' ' + str(nx*ny) + '*1000' +  ' /'
         elif self.neogr and self.upr_rezim_water and self.upr_rezim_gas:
             for j in range(0, nz):
-                if j == 0 or nz-1: 
-                    val=por
-                else:
-                    val=1000
+                val = por 
+                if j == 0: val = 1000
+                if j == nz-1: val = 1000
                 self.por += str(nx+1) + '*' + str(1000) + ' \n'
                 for i in range(0, ny-3):
                     self.por += str(nx-2) + '*' + str(val) + ' \n'
@@ -137,10 +138,8 @@ class ModelGenerator:
                 self.por += str(nx+1) + '*' + str(1000) + ' \n'
         elif self.neogr and self.upr_rezim_water and not self.upr_rezim_gas:
             for j in range(0, nz):
-                if j == nz-1: 
-                    val=por
-                else:
-                    val=1000
+                val = por 
+                if j == nz-1: val = 1000
                 self.por += str(nx+1) + '*' + str(1000) + ' \n'
                 for i in range(0, ny-3):
                     self.por += str(nx-2) + '*' + str(val) + ' \n'
@@ -149,10 +148,8 @@ class ModelGenerator:
                 self.por += str(nx+1) + '*' + str(1000) + ' \n'
         elif self.neogr and not self.upr_rezim_water and self.upr_rezim_gas:
             for j in range(0, nz):
-                if j == 0: 
-                    val=por
-                else:
-                    val=1000
+                val = por 
+                if j == 0: val = 1000
                 self.por += str(nx+1) + '*' + str(1000) + ' \n'
                 for i in range(0, ny-3):
                     self.por += str(nx-2) + '*' + str(val) + ' \n'
@@ -198,9 +195,9 @@ class ModelGenerator:
                                               all_well_z1s, all_well_z2s, skin, rw):
             if template == 2 or template == 4: name = f'"{name}"'
             if horizontal:
-                self.compdat = name + ' ' + str(x) + ' ' + str(y) + ' ' + str(z2) + ' ' + str(z2) + ' OPEN	1*	1* ' + str(rw) +  ' 1* ' + str(skin) + ' 1* X /\n' 
+                self.compdat = name + ' ' + str(x) + ' ' + str(y) + ' ' + str(z2) + ' ' + str(z2) + ' OPEN	1*	1* ' + str(rw) +  ' 1* ' + str(skin) + ' 1* Y /\n' 
                 for i in range(y+1, y_stop[0]+1):
-                    self.compdat += name + ' ' + str(x) + ' ' + str(i) + ' ' + str(z2) + ' ' + str(z2) + ' OPEN	1*	1* ' + str(rw) +  ' 1* ' + str(skin) + ' 1* X /\n'
+                    self.compdat += name + ' ' + str(x) + ' ' + str(i) + ' ' + str(z2) + ' ' + str(z2) + ' OPEN	1*	1* ' + str(rw) +  ' 1* ' + str(skin) + ' 1* Y /\n'
             else:
                 for i in range(0, len(all_well_xs)):
                     self.compdat += name + ' ' + str(all_well_xs[i]) + ' ' + str(all_well_ys[i]) + ' ' + str(all_well_z1s[i]) + ' ' + str(all_well_z2s[i]) + ' OPEN	1*	1*	' + str(rw) +  ' 1* ' + str(skin) + ' /\n'
@@ -233,6 +230,16 @@ class ModelGenerator:
             self.grp_word += f"'PERMY' {k_grp} {xs_start_grp} {xs_stop_grp} {ys_grp} {ys_grp} {nz_grp} {nz_grp} /\n"
             self.grp_word += '/'
 
+        # Моделирование потерь на трение
+        self.welsegs = ''
+        self.compsegs = ''
+        if roughness:
+            for name in zip(all_well_names):
+                self.welsegs += f'"{name}" {tops_depth+z2} 0.0 1* INC HF- /\n'
+                self.welsegs += f'2 {y_stop[0]}  1 1 {y_stop[0]+all_well_z2s[0]} 0 {rw*2} {roughness} /\n'
+                self.compsegs += f'"{name}" /\n'
+                self.compsegs +=  f' {all_well_xs[0]} {all_well_ys[0]} {all_well_z2s[0]} 1 1 1* Y {all_well_ys[-1]} /\n'
+
         # переменные для расчета
         self.result_df = None
         self.fig = None
@@ -260,7 +267,7 @@ class ModelGenerator:
             DX=self.dx, DY=self.dy, DZ=self.dz, TOP_BOX=self.top_box, TOPS=self.tops_depth, PORO_BOX=self.poro_box, PORO=self.por,
             PERMX=self.permx, PERMY=self.permy, PERMZ=self.permz, ROCK=self.rock,  DENSITY=self.density,
             EQUIL=self.equil, WELSPECS=self.welspecs, COMPDAT=self.compdat,
-            WCONPROD=self.wconprod, WCONINJE=self.wconinje, TSTEP=self.tstep, GRP=self.grp_word)
+            WCONPROD=self.wconprod, WCONINJE=self.wconinje, TSTEP=self.tstep, GRP=self.grp_word, WELSEGS=self.welsegs, COMPSEGS=self.compsegs)
 
 
     def create_model(self, name, result_name, keys):
@@ -363,7 +370,7 @@ class ModelGenerator:
         iplot(self.fig_npv)
 
     # метод для отображения графика с оптимальной плотностью сетки
-    def summ_plot_plotn(self, parameters=None, A=None, name=None, l=None):
+    def summ_plot_plotn(self, parameters=None, A=None, name=None, l=None, x_axis=None, y_axis=None, title=None):
         directory = "csv_folder/"
         npv_list = []
         model_list = []
@@ -378,7 +385,15 @@ class ModelGenerator:
             npv_ = self.npv_plotn_method(df, l, A[i])
             npv_list.append(npv_)
             model_list.append(f'Модель: {name[i]}')
+            self.fig.add_trace(go.Scatter(
+                x=df.index, y=y, mode='lines', name=f'Модель: {name[i]}'
+            ))
             i += 1
+
+        self.fig.update_xaxes(tickformat='%d.%m.%y')
+        self.fig.update_layout(title=go.layout.Title(text=title),
+                xaxis_title=x_axis,
+                yaxis_title=y_axis)
         colors = ['lightslategray',] * 6
         colors[2] = 'crimson'
         data = [go.Bar(
@@ -388,12 +403,13 @@ class ModelGenerator:
         self.fig_npv = go.Figure(data=data)
         self.fig_npv.update_layout(title='NPV по моделям')
         iplot(self.fig_npv)
+        iplot(self.fig)
 
     # методы расчета NPV для исследования скважин   
     def npv_method(self, df, l):
         ci = 170*10**6 # руб, капитальные затраты на строительство скважины c поверхностным обустройством;
-        cap_l = 40000 # РУБ, стоимость 1 метра горизонтального ствола;
-        p = 4000 # руб/м3, net-baсk цена нефти за вычетом НПДИ и подготовку нефти; 
+        cap_l = 40000 # 3кк*73/3к=73к РУБ, стоимость 1 метра горизонтального ствола;
+        p = 4500 # 62*73*6,3=28500 # руб/м3, net-baсk цена нефти за вычетом НПДИ и подготовку нефти; 
         opex = 10**6 # руб/год, операционные затраты на скважину;
         r = 0.12 # ставка дисконтирования;
         to = df.index[0]
@@ -416,7 +432,7 @@ class ModelGenerator:
     def npv_plotn_method(self, df, l, A):
         ci = 170*10**6 # руб, капитальные затраты на строительство скважины c поверхностным обустройством;
         cap_l = 40000 # РУБ, стоимость 1 метра горизонтального ствола;
-        p = 4000 # руб/м3, net-baсk цена нефти за вычетом НПДИ и подготовку нефти; 
+        p = 15000 # руб/м3, net-baсk цена нефти за вычетом НПДИ и подготовку нефти; 
         opex = 10**6 # руб/год, операционные затраты на скважину;
         r = 0.12 # ставка дисконтирования;
         to = df.index[0]
@@ -440,27 +456,49 @@ class ModelGenerator:
         self.fig = go.Figure()
         x_opt = []
         y_opt = []
+        ind = [-1, -1, -1, [-5, -5, -6, -5, -5, -6, -5, -5, -5, -5], [-5, -5, -5, -5, -6, -5], -1]
+        x_apr, y_apr = [], []
         for i in range(0, 6):
             P = []
             directory = f"csv_folder/{i}"
             files = [f for f in os.listdir(directory)]
             files.sort(key=lambda x:int(x.split('.')[2]))
-            
+            j = 0
             for file in files:
                 df = pd.read_csv(f'csv_folder/{i}/%s' % file, parse_dates=[0], index_col=[0])
-    
-                val = k*h*(df['FPR'][-1]-df['WBHP:P1'][-1])/(df['WOPR:P1'][-1]*mu)*10**5*86400
-                val = val + h*(10)**0.5/2/3.14/400*(m.log(h*(10)**0.5/(2*3.14*0.5*0.146*(1+(10)**0.5)*m.sin(3.14/2)))+ 0) # 0 в конце это скин
+                if i == 3 or i == 4:
+                    val = k*h*(df['FPR'][ind[i][j]]-df['WBHP:P1'][ind[i][j]])/(df['WOPR:P1'][ind[i][j]]*mu)*10**5*86400
+                else:    
+                    val = k*h*(df['FPR'][ind[i]]-df['WBHP:P1'][ind[i]])/(df['WOPR:P1'][ind[i]]*mu)*10**5*86400
+                val = val + h*(10)**0.5/2/3.14/400*(m.log(h*(10)**0.5/(2*3.14*0.5*0.156*(1+(10)**0.5)*m.sin(3.14/2)))+ 0) # 0 в конце это скин
+                #print(df['WOPR:P1'])
                 P.append(val)
+                j += 1
+            f_interp = interp1d(ls[i] , P, bounds_error=False)
+            x_val = A[i]*10000/(A[i]*10000+160000)
+            # print(x_val)
+            # print(f_interp(x_val))
+            if f_interp(x_val) > 0:
+                x_apr.append(x_val)
+                y_apr.append(f_interp(x_val))
+            self.fig.add_trace(go.Scatter(
+                x=[x_val], y=[f_interp(x_val)], mode='markers', name='Оптимальное соотн. для A/L^2 = ' + str(par[i])
+            ))
             self.fig.add_trace(go.Scatter(
                 x=ls[i],
                 y=P,
                 mode='lines',
                 name='Параметр - A/L^2 = ' + str(par[i])))
+        t = np.polyfit(x_apr, y_apr, 2)
+        f = np.poly1d(t)
+        print('Таким образом, для нашей системы оптимальное соотношение парктически идеально описывается следующим уравнением:')
+        print(f)
+        for i in range(0, 6):
             x_opt.append(A[i]*10000/(A[i]*10000+400**2))
-            y_opt.append(m.log(1+A[i]*10000/(400**2))*4) # убрать *2
-        x_opt.append(384*10000/(384*10000+400**2))
-        y_opt.append(m.log(1+384*10000/(400**2))*4) # убрать *2
+            y_opt.append(f(x_opt[i]))
+        # x_opt.append(200*10000/(200*10000+400**2))
+        # y_opt.append(m.log(1+200*10000/(400**2))*3.5)
+    
         self.fig.add_trace(go.Scatter(
                 x=x_opt,
                 y=y_opt,
@@ -469,7 +507,7 @@ class ModelGenerator:
             ))
         x_axis = 'Соотношение геометрических размеров области дренирования (ширина/длина)'
         y_axis = 'Безразмерный перепад давлений P*'
-        title = 'Безразмерный перепад давления как функция соотношения геометрических размеров области дренирования'
+        title = 'Безразмерный перепад давления как функция соотн. геометрических размеров области дренирования'
         self.fig.update_layout(title=go.layout.Title(text=title),
                                xaxis_title=x_axis,
                                yaxis_title=y_axis)
@@ -505,9 +543,9 @@ class ModelGenerator:
         dframe = grid.df(eclfiles, rstdates=dates)
         df = []
         k = dx
-        for i in range(0, dx):
+        for i in range(0, dy):
             ra = []
-            for j in range(0, dy):
+            for j in range(0, dx):
                 ra.append(dframe[str(parametr)].iloc[j+i*k])
             df.append(ra)
         
@@ -516,7 +554,7 @@ class ModelGenerator:
 
     def grid_plot(self, filename, parametr, title=''):
         df = self.grid_df(filename, parametr, self.grid_dx, self.grid_dy)
-        self.fig = px.imshow(df, labels=dict(x="ny", y="nx", color=title))
+        self.fig = px.imshow(df, labels=dict(x="nx", y="ny", color=title))
         iplot(self.fig)
 
 
